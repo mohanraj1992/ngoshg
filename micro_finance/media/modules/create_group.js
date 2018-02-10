f8ccb3d4ef134fc809959b8699f0251421361ba9
hariharaselvam <hariharaselvam8@gmail.com>
2018-02-10 16:00:04 +0530

    group page loan details

--- micro_finance/media/modules/create_group.js
+++ micro_finance/media/modules/create_group.js
@@ -4,7 +4,32 @@
 window[appName].controller('create_group', function ($rootScope, $stateParams, $scope, $state, $http, fileUpload, $window, $location, $q, $filter) {
 
     $scope.group = {};
+    $scope.loan_details = {};
+    function processTheData(method, action, url, parameter) {
+
+        http.Requests(method, url, parameter).success(function (response) {
+
+
+            switch (action) {
+
+
+                case 'group':
+
+                    $scope.group = response;
+                    break;
+
+
+            }
+        });
+
+    }
+
     $scope.uploadFile = function () {
+
+
+
+        processTheData('post', 'create_loan', '/api/groups/loandetails/create/', $scope.loan_details);
+
         var file = $scope.myFile;
         var uploadUrl = "api/groups/create/";
         fileUpload.uploadFileToUrl(file, $scope.group, uploadUrl);
